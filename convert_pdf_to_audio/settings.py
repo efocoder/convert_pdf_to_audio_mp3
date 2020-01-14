db50831640d0e65ade6864a6108248ccc8937dc6
clemsedem <clemsedem@gmail.com>
2020-01-14 17:48:24 +0000

    lol

--- convert_pdf_to_audio/settings.py
+++ convert_pdf_to_audio/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,3 +126,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
